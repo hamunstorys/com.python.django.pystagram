aa439504fa558347e67d6ab5479778206d742bd5
HongJinseob <hamunstorys@gmail.com>
2017-03-29 13:50:57 +0900

    http://blog.hannal.com/2014/9/start_with_django_webframework_05/ Revision 2 add detail in photos\models.py

--- photos/views.py
+++ photos/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def hello(request):
     return HttpResponse('Hello django!')
+
+
+def detail(request):
+    return HttpResponse('detail 뷰 함수')
--- pystagram/urls.py
+++ pystagram/urls.py
@@ -17,8 +17,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from photos.views import hello
+from photos.views import detail
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello/$', hello),
+    url(r'^photos/(?P<pk>[0-9]+)/$',
+        detail, name='detail'),
 ]
