8740ddc84c73703a89b1693e3c76b63408e41203
HongJinseob <hamunstorys@gmail.com>
2017-03-29 14:10:40 +0900

    http://blog.hannal.com/2015/04/start_with_django_webframework_06/ Revision 2

--- pystagram/settings.py
+++ pystagram/settings.py
@@ -113,10 +113,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATIC_URL = '/assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/upload_files/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
