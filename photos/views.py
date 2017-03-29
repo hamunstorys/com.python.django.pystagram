829464e1904305a353219ab9f0d34ac9d629e459
HongJinseob <hamunstorys@gmail.com>
2017-03-29 13:55:54 +0900

    http://blog.hannal.com/2014/11/start_with_django_webframework_05/ Revision 4 modify detail in photos\models.py

--- photos/views.py
+++ photos/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Photo
 
+
+# root\hello/$
 def hello(request):
     return HttpResponse('Hello django!')
 
 
+# root\photos\$
 def detail(request, pk):
-    msg = '{}번 사진 보여줄게요.'.format(pk)
-    return HttpResponse(msg)
+    photo = Photo.objects.get(pk=pk)
+
+    messages = (
+        '<p>{pk}번 사진 보여줄게요</p>'.format(pk=photo.pk),
+        '<p>주소는 {url}</p>'.format(url=photo.image.url),
+    )
+    return HttpResponse('\n'.join(messages))
