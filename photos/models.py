5602b8e8f6f2d610f2d20dfbfca034850aee398d
HongJinseob <hamunstorys@gmail.com>
2017-03-29 14:01:00 +0900

    http://blog.hannal.com/2014/11/start_with_django_webframework_05/ Revision 7

--- photos/models.py
+++ photos/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Photo(models.Model):
-    image = models.ImageField(upload_to='uploads/%Y/%m/%d/orig')
-    filtered_image = models.ImageField(upload_to='uploads/%Y/%m/%d/filtered')
+    image = models.ImageField(upload_to='%Y/%m/%d/orig')
+    filtered_image = models.ImageField(upload_to='%Y/%m/%d/filtered', blank=True)
     content = models.TextField(max_length=500, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
