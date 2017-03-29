9f2ef2e60b563fc394d55e7850ba137ad8fea202
HongJinseob <hamunstorys@gmail.com>
2017-03-29 15:31:56 +0900

    http://blog.hannal.com/2015/10/start_with_django_webframework_10/ Reivision 1

--- photos/models.py
+++ photos/models.py
@@ -6,15 +6,20 @@ from django.conf import settings
 class Photo(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL)
     image = models.ImageField(upload_to='%Y/%m/%d/orig')
-    filtered_image = models.ImageField(upload_to='%Y/%m/%d/filtered', blank=True)
+    filtered_image = models.ImageField(upload_to='%Y/%m/%d/filtered')
     content = models.TextField(max_length=500, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
-    def delete(self, *args, **kwargs):
-        self.image.delete()
-        self.filtered_image.delete()
-        super(Photo, self).delete(*args, **kwargs)
+    class Meta:
+        ordering = ('-created_at', '-pk',)
 
-    def get_absolute_url(self):
-        url = reverse_lazy('detail', kwargs={'pk': self.pk})
-        return url
+
+def delete(self, *args, **kwargs):
+    self.image.delete()
+    self.filtered_image.delete()
+    super(Photo, self).delete(*args, **kwargs)
+
+
+def get_absolute_url(self):
+    url = reverse_lazy('detail', kwargs={'pk': self.pk})
+    return url
--- pystagram/urls.py
+++ pystagram/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf.urls import include
 
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
@@ -46,6 +47,7 @@ urlpatterns = [
             'next_page': settings.LOGIN_URL,
         }
     ),
+    url(r'^users/', include('profiles.urls')),
 ]
 
 from django.conf.urls.static import static
