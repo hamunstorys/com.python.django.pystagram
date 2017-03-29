c7e74aefa869c3340ba5345a97f3aee82d53cf99
HongJinseob <hamunstorys@gmail.com>
2017-03-29 20:49:34 +0900

    http://blog.hannal.com/2015/04/start_with_django_webframework_06/

---
+++ photos/models.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Photo(models.Model):
+    image = models.ImageField(upload_to='%Y/%m/%d/orig')
+    filtered_image = models.ImageField(upload_to='%Y/%m/%d/filtered')
+    content = models.TextField(max_length=500, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def delete(self, *args, **kwargs):
+        self.image.delete()
+        self.filtered_image.delete()
+        super(Photo, self).delete(*args, **kwargs)
+
